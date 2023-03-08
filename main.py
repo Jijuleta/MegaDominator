33cbe3522e224a6b4444c0f8ac0e224f3f6084c0
VIKTORS KASIHINS <jeyenpm@protonmail.com>
2023-03-08 18:08:34 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from config import API_TOKEN
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='TEST',):
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run(API_TOKEN)
