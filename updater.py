6d21f1a2047da2b15951b985d32c4d9307dc1440
Viktor <jeyenpm@protonmail.com>
2023-03-18 13:47:28 +0200

    2.8.4 change

--- updater.py
+++ updater.py
@@ -1,24 +1,15 @@
-import requests
+import urllib.request
 import os
-import logging
 
-logging.basicConfig(filename='update.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-url = 'https://raw.githubusercontent.com/<username>/<repository>/<branch>/main.py'
+url = 'https://raw.githubusercontent.com/Jijuleta/MegaDominator/master/main.py'
 
 try:
-    response = requests.get(url)
+    urllib.request.urlretrieve(url, 'new_main.py')
 
-    if response.status_code == 200:
-        with open('new_main.py', 'wb') as f:
-            f.write(response.content)
+    os.replace('new_main.py', 'main.py')
 
-        os.replace('new_main.py', 'main.py')
+    os.remove('new_main.py')
 
-        logging.info('Успешное обновление.')
-    else:
-        logging.error('Не удалось скачать обновление.')
-        print('Обновление не удалось.')
-except requests.exceptions.RequestException as e:
-    logging.error(f'Ошибка запроса: {e}')
-    print('Обновление не удалось..')
+    print('Update successful.')
+except urllib.error.URLError as e:
+    print(f'Error downloading file: {e}')
